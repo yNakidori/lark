67b3225ad75dd152b25d3abbd2fe8ae77b1d8d63
Naki <leandroreisubi@gmail.com>
2024-09-10 21:53:11 -0300

    Continuação

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,34 +1,37 @@
-const express = require('express')
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+// configurar o view engine Handlebars
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-})
+const port = process.env.PORT || 3000;
 
-app.get('/about', (req, res) => {
-    res.type('text/plain')
-    res.send('About Meadowlark Travel')
-})
+app.get("/", (req, res) => res.render("home"));
 
-// página 404 peronalizada
+app.get("/about", (req, res) => res, render("about"));
 
+// página 404 personalizada
 app.use((req, res) => {
-    res.type('text/plain')
-    res.status(404)
-    res.send('404 - Not Found')
-})
+  res.status(404);
+  res.render("404");
+});
 
 // página 500 personalizada
-
 app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.type('text/plain')
-    res.status(500)
-    res.send('500 - Server Error')
-})
+  console.error(err.message);
+  res.status(500);
+  res.render("500");
+});
 
-app.listen(port, () => console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`))
+app.listen(port, () =>
+  console.log(
+    `Express started on http://localhost:${port}; press Ctrl-C to terminate.`
+  )
+);
