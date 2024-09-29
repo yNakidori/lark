9585469ddf4c31f3bf3795c5a5944988083524d3
Naki <leandroreisubi@gmail.com>
2024-09-28 23:03:56 -0300

    arquivos estaticos

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,33 +1,37 @@
 const express = require("express");
-const expressHandlebars = require("express-handlebars");
+const { engine } = require("express-handlebars");
 
 const app = express();
 
-// configurar o view engine Handlebars
+// Servir arquivos estáticos da pasta public
+app.get("/about", (req, res) => res.render("about"));
+
+// Configurar o view engine handlebars
 app.engine(
   "handlebars",
-  expressHandlebars({
+  engine({
     defaultLayout: "main",
   })
 );
+app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.render("home"));
 
-app.get("/about", (req, res) => res, render("about"));
-
-// página 404 personalizada
+// Pagina 404
 app.use((req, res) => {
   res.status(404);
   res.render("404");
 });
 
-// página 500 personalizada
+// Pagina 500 personalizada
 app.use((err, req, res, next) => {
   console.error(err.message);
   res.status(500);
-  res.render("500");
+  res, render("500");
 });
 
 app.listen(port, () =>
